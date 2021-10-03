6f1a04a1435b8fa26d8a6e0155f62ae39856ee26
nainwalshubham <nainwalshubham4@gmail.com>
2021-10-03 11:59:50 +0530

    initial-set-up

---
+++ src/main/java/com/Tour/RivaTrip/RivaTripApplication.java
@@ -0,0 +1,13 @@
+package com.Tour.RivaTrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RivaTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RivaTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tour/RivaTrip/RivaTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tour.RivaTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RivaTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
